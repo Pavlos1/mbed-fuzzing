9be27949c1bb675bd9bf4e89247b7c01512c8347
Pavel Zakopaylo <zakopaylo2@gmail.com>
2017-07-02 01:10:46 +1000

    Fixed minor bugs

--- launcher.c
+++ launcher.c
@@ -13,11 +13,12 @@
 void launch_virtual_stm32_ex(char * qemu_executable, char * bin_file, char * sym_file) {
     int pid = fork();
     if (pid < 0) {
-        perror("Failed to fork while trying to execute QEMU\n");
+        fprintf(stderr, "Failed to fork while trying to execute QEMU\n");
         exit(1);
     } else if (pid == 0) {
-        execlp(qemu_executable, "--kernel", bin_file, // ^ data initially loaded into flash
-            "--machine", "stm32-p103",                // ^ at present, the only available STM32 board
+        execlp(qemu_executable, qemu_executable,
+            "-kernel", bin_file,                      // ^ data initially loaded into flash
+            "-machine", "stm32-p103",                 // ^ at present, the only available STM32 board
             "-S",                                     // ^ pause the VM before execution
             "-gdb", "tcp::1234" ,                     // ^ create GDB server, listen on port 1234
             (char *) 0);
@@ -36,11 +37,14 @@ void launch_virtual_stm32(char * qemu_executable, char * bin_file, char * elf_fi
     char * ext = ".sym";
     char * sym_file = malloc(strlen(elf_file) + strlen(path) + strlen(ext) + 1);
     
-    // sym_file = "/tmp/" ++ elf_file ++ ".sym"
+    char * basename_pos = elf_file + strlen(elf_file);
+    while ((basename_pos > elf_file) && (*basename_pos != '/')) basename_pos--;
+    
+    // sym_file = "/tmp/" ++ basename(elf_file) ++ ".sym"
     int index = 0;
     char * ptr = path;
     while (*ptr) sym_file[index++] = *(ptr++);
-    ptr = elf_file;
+    ptr = basename_pos;
     while (*ptr) sym_file[index++] = *(ptr++);
     ptr = ext;
     while (*ptr) sym_file[index++] = *(ptr++);
@@ -48,7 +52,7 @@ void launch_virtual_stm32(char * qemu_executable, char * bin_file, char * elf_fi
     
     int pid = fork();
     if (pid < 0) {
-        perror("Failed to fork while trying to execute arm-none-eabi-objcopy\n");
+        fprintf(stderr, "Failed to fork while trying to execute arm-none-eabi-objcopy\n");
         exit(1);
     } else if (pid == 0) {
         exit(execlp("arm-none-eabi-objcopy", "--only-keep-debug", elf_file, sym_file, (char *) 0));
@@ -56,16 +60,16 @@ void launch_virtual_stm32(char * qemu_executable, char * bin_file, char * elf_fi
     
     int status;
     if(waitpid(pid, &status, 0) < 0) {
-        perror("Error in waitpid() while trying to execute arm-none-eabi-objcopy\n");
+        fprintf(stderr, "Error in waitpid() while trying to execute arm-none-eabi-objcopy\n");
         exit(1);
     }
     if (!WIFEXITED(status)) {
-        perror("Child process never exited (this should never happen?) while trying"
+        fprintf(stderr, "Child process never exited (this should never happen?) while trying"
             " to execute arm-none-eabi-objcopy\n");
         exit(1);
     }
-    if (!WEXITSTATUS(status)) {
-        perror("arm-none-eabi-objcopy returned with non-zero exit status\n");
+    if (WEXITSTATUS(status)) {
+        fprintf(stderr, "arm-none-eabi-objcopy returned with non-zero exit status: %d\n", WEXITSTATUS(status));
         exit(1);
     }
     
